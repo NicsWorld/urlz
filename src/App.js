ae454949ecd5f4443f61d32aced344fca739cce5
NicsWorld <nicwoods63@gmail.com>
2022-09-07 21:06:54 -0500

    Remove placeholder

--- src/App.js
+++ src/App.js
@@ -309,7 +309,6 @@ function App() {
               disabled
               value={finalUrl}
               onChange={() => {}}
-              // placeholder="https://www.google.com/search?q=google+cat+in+a+hat+photo&rlz=1C1CHBF_enUS985US985&oq=google+cat+in+a+hat+photo&aqs=chrome..69i57j69i64.6799j0j1&sourceid=chrome&ie=UTF-8"
             />
             <button
               onClick={() => {
